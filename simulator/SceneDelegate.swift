7ba6b4b1897049e2de9d18ed74990314539abda4
Aaron Anderson <aaronanderson@iheartmedia.com>
2020-03-23 16:43:58 -0500

    add web view

--- simulator/SceneDelegate.swift
+++ simulator/SceneDelegate.swift
@@ -20,7 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        // update the url for debugging specific pages
+        let url = "https://iheart.com"
+        let contentView = WebView(request: URLRequest(url: URL(string: url)!))
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
